73ed185a18b1a00b69269e256f3ec1acb74122c8
rankun <rankun@bytedance.com>
2020-04-05 00:08:10 +0800

    feat: use mymath option
    Change-Id: I2c56fa0682362b6ee276c34f452d5b27a0284104

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <stdio.h>
 
+#ifdef USE_MYMATH
 #include "mymath.h"
+#else
+#include <math.h>
+#endif
+#include "Config.h"
 
 int main()
 {
-  std::cout << "Hello world 4 sqrt is " << mysqrt(4) << std::endl;
-  getchar();
+    std::cout << "Hello world 4 sqrt is "
+#ifdef USE_MYMATH
+    << mysqrt(4) 
+#else
+    << sqrt(4) 
+#endif
+    << std::endl;
+
+    getchar();
 }
