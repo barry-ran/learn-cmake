d4c67c38cf031cabd5f829c692663f8dfd5b3ca8
rankun <rankun@bytedance.com>
2020-04-05 11:17:35 +0800

    fix: build error
    Change-Id: I5762574e08b2cda9fe706e75d036c8b25c41f091

--- mymath/mymath.cpp
+++ mymath/mymath.cpp
@@ -5,7 +5,7 @@ double mysqrt(double input) {
     std::cout << "call mysqrt" << std::endl;
 #if defined(HAVE_LOG) && defined(HAVE_EXP)
     double result = exp(log(input) * 0.5);
-    std::cout << "Computing sqrt of " << x << " to be " << result
+    std::cout << "Computing sqrt of " << input << " to be " << result
             << " using log and exp" << std::endl;
     return result;
 #else
