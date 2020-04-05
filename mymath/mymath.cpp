3acafed1f8bc5b644436a99d4f85156a437a293f
rankun <rankun@bytedance.com>
2020-04-05 10:53:03 +0800

    feat: check symbol
    Change-Id: Ic30611e5d2e4c17c3030b4a19fe75ca93ec7d100

--- mymath/mymath.cpp
+++ mymath/mymath.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 double mysqrt(double input) {
     std::cout << "call mysqrt" << std::endl;
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+    double result = exp(log(x) * 0.5);
+    std::cout << "Computing sqrt of " << x << " to be " << result
+            << " using log and exp" << std::endl;
+    return result;
+#else
     return sqrt(input);
+#endif
 }
